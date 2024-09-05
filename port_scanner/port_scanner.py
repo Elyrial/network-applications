4746444903bd2ec2bfc007305e7d46e36b3f57ca
Elyrial <elyrial@proton.me>
2024-09-05 20:40:08 +0200

    Add summarize list for open ports
    Added a list that adds all the open ports and summarize them after all
    ports have been checked.

--- port_scanner/port_scanner.py
+++ port_scanner/port_scanner.py
@@ -28,6 +28,9 @@ def is_port_open(host, port):
         return True
 
 def main():
+    # Empty list to store hosts with open port
+    open_port_list = []
+
     host_address = input("Enter the host (e.g., localhost or google.com): ")
 
     # Iterate ports from 1 to 512
@@ -35,8 +38,13 @@ def main():
         # Only prints out when the port is open
         if is_port_open(host_address, port):
             print(f'{GREEN}[+] {host_address}:{port} is open {RESET}')
+            open_port_list.append(port)
         else:
             print(f'{GRAY}[!] {host_address}:{port} is closed {RESET}')
+
+    # Print out all open ports
+    for index, open_port in enumerate(open_port_list, start=1):
+        print(f'{index}. PORT {GREEN}{open_port}{RESET} is open')
 
 if __name__ == "__main__":
     main()
